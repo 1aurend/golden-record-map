24758e9cbbd771c2b671f62946843f4e490a467b
Lauren Davidson <ldavidson@g.harvard.edu>
2020-05-22 13:46:40 -0700

    Add music data

---
+++ src/music.js
@@ -0,0 +1,245 @@
+const music = [
+ {
+   "Country": "Germany",
+   "Piece": "Brandenburg Concerto No. 2 in F Major, BWV 1047: I. Allegro",
+   "Composer": "Johann Sebastian Bach",
+   "Performer(s)": "Munich Bach Orchestra/Karl Richter (conductor) featuring Karl-Heinz Schneeberger (violin)",
+   "Recorded by / in": "Recorded in Munich, Germany, January 1967",
+   "Genre of music": "Classical music / Baroque music",
+   "Length": "4:40"
+ },
+ {
+   "Country": "Indonesia",
+   "Piece": "Ketawang: Puspåwårnå (Kinds of Flowers)",
+   "Composer": "Mangkunegara IV",
+   "Performer(s)": "Pura Paku Alaman Palace Orchestra/K.R.T. Wasitodipuro (director) featuring Niken Larasati and Nji Tasri (vocals)",
+   "Recorded by / in": "Recorded by Robert E. Brown in Yogyakarta, Java, Indonesia, on January 10, 1971",
+   "Genre of music": "Folk music / Gamelan",
+   "Length": "4:43"
+ },
+ {
+   "Country": "Benin",
+   "Piece": "Cengunmé[18]",
+   "Composer": "Traditional",
+   "Performer(s)": "Mahi musicians of Benin",
+   "Recorded by / in": "Recorded by Charles Duvelle in Savalou, Benin, West Africa, January 1963",
+   "Genre of music": "Folk music / Percussion",
+   "Length": "2:08"
+ },
+ {
+   "Country": "Democratic Republic of Congo",
+   "Piece": "Alima Song",
+   "Composer": "Traditional",
+   "Performer(s)": "Mbuti of the Ituri Rainforest",
+   "Recorded by / in": "Recorded by Colin Turnbull and Francis S. Chapman in the Ituri Rainforest of the Democratic Republic of Congo, circa 1951",
+   "Genre of music": "Folk music",
+   "Length": "0:56"
+ },
+ {
+   "Country": "Australia",
+   "Piece": "Barnumbirr (Morning Star) and Moikoi Song",
+   "Composer": "Traditional",
+   "Performer(s)": "Tom Djawa (clapsticks), Mudpo (digeridoo), and Waliparu (vocals)[19]",
+   "Recorded by / in": "Recorded by Sandra LeBrun Holmes at Milingimbi Mission on Milingimbi Island, off the coast of Arnhem Land, Northern Territory, Australia, 1962",
+   "Genre of music": "Folk music / Indigenous music of Australia",
+   "Length": "1:26"
+ },
+ {
+   "Country": "Mexico",
+   "Piece": "El Cascabel (The Bell)",
+   "Composer": "Lorenzo Barcelata",
+   "Performer(s)": "Antonio Maciel and Los Aguilillas with Mariachi México de Pepe Villa/Rafael Carrión (conductor)",
+   "Recorded by / in": "Recorded 1957, Musart Records",
+   "Genre of music": "Mariachi",
+   "Length": "3:14"
+ },
+ {
+   "Country": "USA",
+   "Piece": "Johnny B. Goode",
+   "Composer": "Chuck Berry",
+   "Performer(s)": "Chuck Berry (vocals, guitar) with Lafayette Leak (piano), Willie Dixon (bass), and Fred Below (drums)",
+   "Recorded by / in": "Recorded at Chess Studios, Chicago, Illinois, on January 6, 1958",
+   "Genre of music": "Rock and roll",
+   "Length": "2:03"
+ },
+ {
+   "Country": "Papua New Guinea",
+   "Piece": "Mariuamangɨ",
+   "Composer": "Traditional",
+   "Performer(s)": "Pranis Pandang and Kumbui (mariuamangɨ) of the Nyaura Clan",
+   "Recorded by / in": "Recorded by Robert MacLennan in the village of Kandɨngei, Middle Sepik, Papua New Guinea, on July 23, 1964",
+   "Genre of music": "Folk music",
+   "Length": "1:20"
+ },
+ {
+   "Country": "Japan / USA",
+   "Piece": "Sokaku-Reibo (Depicting The Cranes In Their Nest)",
+   "Composer": "Arranged by Kinko Kurosawa",
+   "Performer(s)": "Goro Yamaguchi (shakuhachi)",
+   "Recorded by / in": "Recorded in New York City, circa 1967, Elektra Entertainment",
+   "Genre of music": "Folk music /Honkyoku",
+   "Length": "4:51"
+ },
+ {
+   "Country": "Germany / Belgium",
+   "Piece": "Partita for Violin Solo No. 3 in E Major, BWV 1006: III. Gavotte En Rondeau",
+   "Composer": "Johann Sebastian Bach",
+   "Performer(s)": "Arthur Grumiaux (violin)",
+   "Recorded by / in": "Recorded in Berlin, Germany, November 1960, Decca Music Group Limited",
+   "Genre of music": "Classical music / Baroque music",
+   "Length": "2:55"
+ },
+ {
+   "Country": "Austria / Germany",
+   "Piece": "The Magic Flute (Die Zauberflöte), K. 620, Act II: Hell's Vengeance Boils In My Heart",
+   "Composer": "Wolfgang Amadeus Mozart",
+   "Performer(s)": "Bavarian State Opera Orchestra and Chorus/Wolfgang Sawallisch (conductor) featuring Edda Moser (soprano)",
+   "Recorded by / in": "Recorded in Munich, Germany, August 1972",
+   "Genre of music": "Classical music / Opera / Singspiel",
+   "Length": "2:55"
+ },
+ {
+   "Country": "Georgia",
+   "Piece": "Chakrulo",
+   "Composer": "Traditional",
+   "Performer(s)": "Georgian State Merited Ensemble of Folk Song and Dance/Anzor  Kavsadze (director) featuring Ilia Zakaidze (first tenor) and Rostom  Saginashvili (second tenor)[20][21]",
+   "Recorded by / in": "Recorded at Melodiya Studio in Tbilisi, Georgia",
+   "Genre of music": "Folk music / Choral music",
+   "Length": "2:18"
+ },
+ {
+   "Country": "Peru",
+   "Piece": "Roncadoras and Drums",
+   "Composer": "Traditional",
+   "Performer(s)": "Musicians from Ancash",
+   "Recorded by / in": "From recordings collected by Jose Maria Arguedas (Casa de la Cultura) in the Ancash Region of Peru, 1964",
+   "Genre of music": "Folk music",
+   "Length": "0:52"
+ },
+ {
+   "Country": "USA",
+   "Piece": "Melancholy Blues",
+   "Composer": "Marty Bloom and Walter Melrose",
+   "Performer(s)": "Louis Armstrong and His Hot Seven",
+   "Recorded by / in": "Recorded in Chicago, Illinois, on May 11, 1927",
+   "Genre of music": "Jazz",
+   "Length": "3:05"
+ },
+ {
+   "Country": "Azerbaijan S.S.R.",
+   "Piece": "Muğam (Çahargah ahəngi)",
+   "Composer": "Traditional",
+   "Performer(s)": "Kamil Jalilov (balaban)",
+   "Recorded by / in": "Recorded by Radio Moscow, circa 1950",
+   "Genre of music": "Folk music",
+   "Length": "2:30"
+ },
+ {
+   "Country": "Soviet Union / USA",
+   "Piece": "The Rite of Spring (Le Sacre Du Printemps), Part II-The Sacrifice: VI. Sacrificial Dance (The Chosen One)",
+   "Composer": "Igor Stravinsky",
+   "Performer(s)": "Columbia Symphony Orchestra/Igor Stravinsky (conductor)",
+   "Recorded by / in": "Recorded at the Ballroom of the St. George Hotel, Brooklyn, New York, on January 6, 1960",
+   "Genre of music": "Modern classical music / Ballet",
+   "Length": "4:35"
+ },
+ {
+   "Country": "Germany / Canada / USA",
+   "Piece": "The Well-Tempered Clavier, Book II, Prelude and Fugue in C major, BWV 870",
+   "Composer": "Johann Sebastian Bach",
+   "Performer(s)": "Glenn Gould (piano)",
+   "Recorded by / in": "Recorded at CBS 30th Street Studio in New York City on August 8, 1966",
+   "Genre of music": "Classical music / Baroque music",
+   "Length": "4:48"
+ },
+ {
+   "Country": "Germany / UK",
+   "Piece": "Symphony No. 5 in C minor, Opus 67: I. Allegro Con Brio",
+   "Composer": "Ludwig van Beethoven",
+   "Performer(s)": "Philharmonia Orchestra/Otto Klemperer (conductor)",
+   "Recorded by / in": "Recorded at Kingsway Hall, London, on October 6, 1955",
+   "Genre of music": "Classical music / Romantic music",
+   "Length": "7:20"
+ },
+ {
+   "Country": "Bulgaria",
+   "Piece": "Izlel E Delyo Haydutin",
+   "Composer": "Traditional",
+   "Performer(s)": "Valya Balkanska (vocal), Lazar Kanevski, and Stephan Zahmanov (kaba gaidi)",
+   "Recorded by / in": "Recorded by Martin Koenig and Ethel Rain in Smolyan, Bulgaria, 1968",
+   "Genre of music": "Folk music",
+   "Length": "4:59"
+ },
+ {
+   "Country": "USA",
+   "Piece": "Navajo Night Chant, Yeibichai Dance",
+   "Composer": "Traditional",
+   "Performer(s)": "Ambrose Roan Horse, Chester Roan, and Tom Roan",
+   "Recorded by / in": "Recorded by Willard Rhodes in Pine Springs, Arizona, Summer 1942",
+   "Genre of music": "Folk music",
+   "Length": "0:57"
+ },
+ {
+   "Country": "UK",
+   "Piece": "The Fairie Round",
+   "Composer": "Anthony Holborne",
+   "Performer(s)": "Early Music Consort of London/David Munrow (director)",
+   "Recorded by / in": "Recorded at Abbey Road Studios, London, September 1973",
+   "Genre of music": "Classical music",
+   "Length": "1:17"
+ },
+ {
+   "Country": "Solomon Islands",
+   "Piece": "Naranaratana Kookokoo (The Cry of the Megapode Bird)",
+   "Composer": "Traditional",
+   "Performer(s)": "Maniasinimae and Taumaetarau Chieftain Tribe of Oloha and Palasu'u Village Community in Small Malaita",
+   "Recorded by / in": "Recording of Solomon Islands Broadcasting Services (SIBS)",
+   "Genre of music": "Folk music",
+   "Length": "1:12"
+ },
+ {
+   "Country": "Peru",
+   "Piece": "Wedding Song",
+   "Composer": "Traditional",
+   "Performer(s)": "Performed by young girl of Huancavelica",
+   "Recorded by / in": "Recorded by John and Penny Cohen in Huancavelica, Peru, 1964",
+   "Genre of music": "Folk music",
+   "Length": "0:38"
+ },
+ {
+   "Country": "China",
+   "Piece": "Liu Shui (Flowing Streams)",
+   "Composer": "Bo Ya",
+   "Performer(s)": "Guan Pinghu (guqin)",
+   "Recorded by / in": "Recording of Smithsonian Folkways Recordings",
+   "Genre of music": "Folk music",
+   "Length": "7:37"
+ },
+ {
+   "Country": "India",
+   "Piece": "Bhairavi: Jaat Kahan Ho",
+   "Composer": "Traditional",
+   "Performer(s)": "Kesarbai Kerkar (vocals) with harmonium and tabla accompaniment",
+   "Recorded by / in": "Recorded in Bombay, India, April 1953",
+   "Genre of music": "Folk music",
+   "Length": "3:30"
+ },
+ {
+   "Country": "USA",
+   "Piece": "Dark Was The Night, Cold Was The Ground",
+   "Composer": "Blind Willie Johnson",
+   "Performer(s)": "Blind Willie Johnson (slide guitar, vocals)",
+   "Recorded by / in": "Recorded in Dallas, Texas, on December 3, 1927",
+   "Genre of music": "Blues",
+   "Length": "3:15"
+ },
+ {
+   "Country": "Germany / Hungary / USA",
+   "Piece": "String Quartet No. 13: in B-Flat Major, Opus 130: V. Cavatina",
+   "Composer": "Ludwig van Beethoven",
+   "Performer(s)": "Budapest String Quartet",
+   "Recorded by / in": "Recorded at the Library of Congress, Washington, D.C., on April 7, 1960",
+   "Genre of music": "Classical music / Romantic music",
+   "Length": "6:37"
+ }
+]
